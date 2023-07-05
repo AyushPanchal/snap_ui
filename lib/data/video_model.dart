b3f7f3facf96213abbe10fa9cc8c56c24a667c77
Ayush <ayush18november@gmail.com>
2023-07-05 13:04:05 +0530

    created video model to store meta data about video and url

---
+++ lib/data/video_model.dart
@@ -0,0 +1,12 @@
+class VideoModel {
+  final String likes;
+  final String shares;
+  final String comments;
+  final String videoUrl;
+
+  VideoModel(
+      {required this.likes,
+      required this.shares,
+      required this.comments,
+      required this.videoUrl});
+}
