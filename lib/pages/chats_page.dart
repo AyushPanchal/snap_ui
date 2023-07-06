d5c858e400dcd0ce951405fb51c58879ca842c5b
Ayush <ayush18november@gmail.com>
2023-07-06 18:02:54 +0530

    custom switches created on chats page

--- lib/pages/chats_page.dart
+++ lib/pages/chats_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:snap/theme/colors.dart';
@@ -13,64 +14,124 @@ class ChatsPage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
-              padding: EdgeInsets.only(
-                left: 8.w,
-                right: 8.w,
-                top: 5.h,
+            _appBar(),
+            Container(
+              margin: EdgeInsets.only(
+                left: 5.w,
+                top: 18.h,
               ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      CustomIconButton(
-                        child: Image.asset('assets/boy.png'),
-                      ),
-                      SizedBox(
-                        width: 3.w,
-                      ),
-                      CustomIconButton(
+              height: 28.h,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: switchIcons.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 3.w),
+                      child: CustomSwitch(
                         child: Icon(
-                          Icons.search_rounded,
-                          size: 24.h,
+                          switchIcons[index],
+                          size: 20.sp,
                           color: kIconColor,
                         ),
                       ),
-                    ],
-                  ),
-                  Text(
-                    "Chats",
-                    style: kPageTitleTextStyle,
-                  ),
-                  Row(
-                    children: [
-                      CustomIconButton(
-                        child: Icon(
-                          Icons.person_add_alt_1_rounded,
-                          size: 24.h,
-                          color: kIconColor,
-                        ),
-                      ),
-                      SizedBox(
-                        width: 5.w,
-                      ),
-                      CustomIconButton(
-                        child: Icon(
-                          Icons.more_horiz_rounded,
-                          size: 24.h,
-                          color: kIconColor,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
+                    );
+                  }),
+            )
           ],
         ),
       ),
     );
   }
+
+  Padding _appBar() {
+    return Padding(
+      padding: EdgeInsets.only(
+        left: 8.w,
+        right: 8.w,
+        top: 5.h,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              CustomIconButton(
+                child: Image.asset('assets/boy.png'),
+              ),
+              SizedBox(
+                width: 3.w,
+              ),
+              CustomIconButton(
+                child: Icon(
+                  Icons.search_rounded,
+                  size: 24.h,
+                  color: kIconColor,
+                ),
+              ),
+            ],
+          ),
+          Text(
+            "Chats",
+            style: kPageTitleTextStyle,
+          ),
+          Row(
+            children: [
+              CustomIconButton(
+                child: Icon(
+                  Icons.person_add_alt_1_rounded,
+                  size: 24.h,
+                  color: kIconColor,
+                ),
+              ),
+              SizedBox(
+                width: 5.w,
+              ),
+              CustomIconButton(
+                child: Icon(
+                  Icons.more_horiz_rounded,
+                  size: 24.h,
+                  color: kIconColor,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 }
+
+class CustomSwitch extends StatelessWidget {
+  final Widget child;
+  const CustomSwitch({
+    super.key,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 15.w,
+        vertical: 3.h,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(
+          18.r,
+        ),
+        color: kSwitchBGColor,
+      ),
+      child: child,
+    );
+  }
+}
+
+List<IconData> switchIcons = [
+  Icons.chat_rounded,
+  CupertinoIcons.chat_bubble_2_fill,
+  Icons.groups_rounded,
+  Icons.emoji_emotions_rounded,
+  CupertinoIcons.plus,
+];
--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -7,4 +7,5 @@ const Color kPageTitleTextColor = Color(0XFF15191C);
 const Color kBottomNavBarBGColor = Color(0XFFEEEEEE);
 const Color kFriendsBGColor = Color(0XFFE5EDEF);
 const Color kPurpleColor = Color(0XFFA05DCE);
+const Color kSwitchBGColor = Color(0XFFEAEFF2);
 const Color kFriendUserNameColor = Color(0XFF9C9FA4);
